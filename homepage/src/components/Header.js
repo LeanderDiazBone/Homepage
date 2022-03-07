2f57d3b00d2821eba0fec01baca8f2ac54170698
Leander Diaz-Bone <leanderdiazbone@gmail.com>
2022-03-07 22:15:02 +0100

    started on a responsive header

--- homepage/src/components/Header.js
+++ homepage/src/components/Header.js
@@ -2,11 +2,13 @@ import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
-        <header className = '.header'>
-           <Link to="/" id = "headerlink">Home</Link>
-           <Link to="/projects" id = "headerlink">Projects</Link>
-           <Link to="/blog" id = "headerlink">Blog</Link>
-           <Link to="/about" id = "headerlink">About</Link>
+        <header className = 'header'>
+            <Link to="/" id = "headerlink"><img src={ require('../images/GitHub-Logo.png') }/></Link>
+            <nav className="Nav">
+            <Link to="/projects" id = "headerlink">Projects</Link>
+            <Link to="/blog" id = "headerlink">Blog</Link>
+            <Link to="/about" id = "headerlink">About</Link>
+           </nav>
             <a href = "https://github.com/LeanderDiazBone" ><img id="iconlink" src={ require('../images/GitHub-Logo.png') } alt="GitHub Logo"/></a>
             <a href = "https://www.linkedin.com/in/leanderdiazbone/" id="iconlink"><img id="iconlink" src={ require('../images/Linkedin-Logo.png') } alt="LinkedIn Logo"/></a>
             <a href = "" id="iconlink"><img id="iconlink" src={ require('../images/Mail-Logo.png') } alt="Mail Logo"/></a>
